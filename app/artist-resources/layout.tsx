2e726c3f8c71f93a6edfc79c42cb7f362fd142b5
Chad OBrien <obrien.music@gmail.com>
2024-01-18 21:03:43 -0500

    initial backend and frontend structure of auth0

---
+++ app/artist-resources/layout.tsx
@@ -0,0 +1,9 @@
+import { UserProvider } from '@auth0/nextjs-auth0/client';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<UserProvider>
+			<>{children}</>
+		</UserProvider>
+	)
+}
